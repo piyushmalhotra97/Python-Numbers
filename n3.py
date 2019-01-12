b9aa1824af2ebca52485cb778dd293020dcf7d35
Piyush Malhotra <46517096+piyushmalhotra97@users.noreply.github.com>
2019-01-12 14:41:07 +0530

    Add files via upload

---
+++ n3.py
@@ -0,0 +1,6 @@
+#max function and min function
+print'max(50,64,796)',max(50,64,796)
+print'max(-34,-20,-45)',max(-34,-20,-45)
+
+print'min(50,64,46)',min(50,64,46)
+print'min(-34,-20,-45)',min(-34,-20,-45) 
---
+++ n4.py
@@ -0,0 +1,3 @@
+import math
+print math.pow(16,2)
+print math.pow(6,-3)
